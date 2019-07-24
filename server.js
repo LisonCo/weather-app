3eb270c56e8a6b1756b6ad92759f75c1eeff9f07
Lison <lisoncouturier@gmail.com>
2019-07-24 12:20:34 +0200

    done

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // https://codeburst.io/build-a-weather-website-in-30-minutes-with-node-js-express-openweather-a317f904897b
+require('dotenv').config()
 
 const express = require('express')
 const app = express()
 const hbs = require("hbs");
 const bodyParser = require('body-parser');
+const request = require('request');
+const apiKey = process.env.apiKey;
 
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
@@ -14,8 +17,21 @@ app.get('/', function (req, res) {
 })
 
 app.post('/', function (req, res) {
-  res.render('index');
-  console.log(req.body.city);
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
 })
 
 app.listen(3000, function () {
